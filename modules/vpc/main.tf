ea9893ea0ff1774e357f18572e95e49b8611a9b4
Lucas Pearson <catdevman@gmail.com>
2018-10-24 21:17:52 -0400

    Update VPC module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -2,7 +2,7 @@ module "network_label" {
   source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=master"
   namespace  = "${var.namespace}"
   stage      = "${var.stage}"
-  name       = "${var.application}-vpc"
+  name       = "vpc"
   delimiter  = "-"
   tags = "${var.tags}"
 }
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,43 +1,63 @@
+variable "region" {
+  description = "Region to create this vpc in."
+  type = "string"
+}
+
 variable "cidr" {
-  description = "Cidr block range for this vpc (ex: 10.0.0.0/16)"
+  description = "CDIR block range for this vpc. (ex: 10.0.0.0/16)"
   type = "string"
 }
 
 variable "availability_zones" {
-  description = "Availablility zones for this vpc"
+  description = "Availablility zones for this vpc."
   type = "list"
 }
 
 variable "private_subnets" {
-  description = "Availablility zones for this vpc"
+  description = "Availablility zones for this vpc."
   type = "list"
 }
 
 variable "public_subnets" {
-  description = "Availablility zones for this vpc"
+  description = "Availablility zones for this vpc."
   type = "list"
 }
 
 variable "enable_nat_gateway" {
-  description = "Should be true if you want to provision NAT Gateways for each of your private networks"
-  type = "boolean"
+  description = "Should be true if you want to provision NAT Gateways for each of your private networks."
+  default = true
 }
 variable "single_nat_gateway" {
-  description = "Should be true if you want to provision a single shared NAT Gateway across all of your private networks"
-  type = "boolean"
+  description = "Should be true if you want to provision a single shared NAT Gateway across all of your private networks."
+  default = true
 }
 
 variable "enable_dns_support" {
-  description = "Should be true to enable DNS support in the VPC"
-  type = "boolean"
+  description = "Should be true to enable DNS support in the VPC."
+  default = true
 }
 variable "enable_dns_hostnames" {
-  description = "Should be true to enable DNS hostnames in the VPC"
-  type = "boolean"
+  description = "Should be true to enable DNS hostnames in the VPC."
+  default = true
+}
+
+variable "namespace" {
+  description = "Namespace"
+  type = "string"
+}
+
+variable "application" {
+  description = "Application"
+  type = "string"
+}
+
+variable "stage" {
+  description = "Stage"
+  type = "string"
 }
 
 variable "tags" {
-  description = "A map of tags to add to all resources"
+  description = "A map of tags to add to all resources."
   default = {}
   type = "map"
 }
